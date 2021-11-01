9f040168ef942aead68cbc7dd6be8f3a012ae56d
Nikolay Neupokoev <neupokoev.n@gmail.com>
2021-11-01 07:38:24 -0700

    Rabbit recursion by shafikyaghmour
    https://twitter.com/shafikyaghmour/status/1449735918487674882

---
+++ rabbit/holding.cpp
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+    (\_/)
+    ( •_•)
+    / >[x= (\_/)
+           ( •_•)
+            / >](auto thingToHold)
+    {
+        return x(thingToHold);
+    };
+}
