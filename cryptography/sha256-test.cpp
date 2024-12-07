b072c5ae64c318572d898b025c3c77f94b1c0f56
Nikolay Neupokoev <neupokoev.n@gmail.com>
2024-12-06 16:33:56 -0800

    Add SHA256 checksum v3 version

--- cryptography/sha256-test.cpp
+++ cryptography/sha256-test.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 // #include <list>
 #include <openssl/evp.h>
 #include <openssl/sha.h>
@@ -8,7 +9,32 @@
 #include <string>
 #include <vector>
 
-std::string sha256(const unsigned char* buffer, size_t size)
+std::string sha256_v3(const unsigned char* buffer, size_t size)
+{
+    const EVP_MD *message_digest = EVP_get_digestbyname("SHA256");
+    if (message_digest == NULL)
+        std::cerr << "SHA256 unknown message digest" << std::endl;
+
+    EVP_MD_CTX *md_context = EVP_MD_CTX_new();
+    EVP_DigestInit_ex2(md_context, message_digest, NULL);
+    
+    if (EVP_DigestUpdate(md_context, buffer, size) == 0)
+        std::cerr << "SHA256 update failed" << std::endl;
+    
+    std::array<unsigned char, EVP_MAX_MD_SIZE> digest;
+    digest.fill('\0');
+    unsigned int digest_length = 0;
+    if (EVP_DigestFinal_ex(md_context, digest.data(), &digest_length) == 0)
+        std::cerr << "SHA256 final failed" << std::endl;
+
+    std::ostringstream ss;
+    for (unsigned int i = 0; i < digest_length; ++i) {
+        ss << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(digest[i]);
+    }
+    return ss.str();
+}
+
+std::string sha256_v1(const unsigned char* buffer, size_t size)
 {
     SHA256_CTX context;
     if (SHA256_Init(&context) == 0)
@@ -24,7 +50,7 @@ std::string sha256(const unsigned char* buffer, size_t size)
     
     std::ostringstream ss;
     for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
-        ss << std::hex << static_cast<int>(digest[i]);
+        ss << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(digest[i]);
     }
     return ss.str();
 }
@@ -41,7 +67,8 @@ int main(int argc, char const *argv[])
     unsigned char* buffer = reinterpret_cast<unsigned char*>(std::malloc(buffer_size));
     file.read(reinterpret_cast<char*>(buffer), buffer_size);
 
-    std::cout << "SHA256: " << sha256(buffer, buffer_size) << std::endl;
+    std::cout << "SHA256 v1: " << sha256_v1(buffer, buffer_size) << std::endl;
+    std::cout << "SHA256 v3: " << sha256_v3(buffer, buffer_size) << std::endl;
     std::free(buffer);
 
     return 0;
--- src/crypto/sha.md
+++ src/crypto/sha.md
@@ -1,5 +1,13 @@
 # SHA256
 
+## OpenSSL v3
+
 ```cpp
-{{#include ../../cryptography/sha256-test.cpp}}
+{{#include ../../cryptography/sha256-test.cpp:12:35}}
+```
+
+## OpenSSL v1
+
+```cpp
+{{#include ../../cryptography/sha256-test.cpp:37:56}}
 ```
